fd850f564e932cca7036725ee4973067af33d420
sureshnanjan <sureshnanjan@outlook.com>
2025-12-17 08:23:48 +0530

    Making changes for framework structure

--- colorful.js
+++
@@ -1,6 +0,0 @@
-// npm install chalk
-const chalk = require('chalk');
-console.log(chalk)  
-console.log(chalk.green('✓ Success!')); 
-console.log(chalk.red('✗ Error!')); 
-console.log(chalk.blue('ℹ Info!'));
