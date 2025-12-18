d181abf5ed1f435dc9297122c8077f798c742c9e
Supraja1992 <suprajapuligandala@gmail.com>
2025-12-18 09:48:00 +0530

    Updated mcart and  mobile enum code

---
+++ src/mcart-types.js
@@ -0,0 +1,29 @@
+// Initial variable declarations
+var productId = 101;
+var productDescription = "Wireless Bluetooth Earbuds";
+//let productName: string = "Sony WF-C500";
+// Activity 1: Modify Data Type 
+// • Change the data type of productName from string to number a 
+//• Re-execute the program and observe the output.
+var productName = 5001;
+var productAvailable = true;
+var discountPercentage = 20;
+/*Activity 2: Update Variable Values
+ 
+perform the following updates:
+• Reassign productAvailable to false
+• Reassign discountPercentage to 30
+• Print the updated values on the console
+• Re-execute the code and verify the changes
+*/
+productAvailable = false;
+var updatedDiscountPercentage = 30;
+// Print the data types
+console.log("Type of productId:", typeof productId);
+console.log("Type of productDescription:", typeof productDescription);
+console.log("Type of productName:", typeof productName);
+//console.log("Type of productAvailable:", typeof productAvailable);
+console.log("Product Available:", productAvailable);
+// Print the discount value
+//console.log("Discount Percentage:", discountPercentage);
+console.log("Updated Discount Percentage:", updatedDiscountPercentage);
