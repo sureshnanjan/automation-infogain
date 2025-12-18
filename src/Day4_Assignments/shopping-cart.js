c51b430bf0c38650f28f054a9e8c2137c61c3115
Supraja1992 <suprajapuligandala@gmail.com>
2025-12-18 18:50:24 +0530

    Added files

---
+++ src/Day4_Assignments/shopping-cart.js
@@ -0,0 +1,11 @@
+// Cart can store only string values (product names)
+var cart = [];
+function addToCart(productNames) {
+    for (var _i = 0, productNames_1 = productNames; _i < productNames_1.length; _i++) {
+        var product = productNames_1[_i];
+        cart.push(product);
+    }
+    return cart;
+}
+console.log("Cart Items are: " +
+    addToCart(["Moto G Play, 4th Gen", "Apple iPhone 5s"]).join(", "));
