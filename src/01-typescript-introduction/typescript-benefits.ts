9ca787d31591146a76c52298341ba2a5ab60ad9c
Chilaka Anusha Rani <Chilaka.Rani@igglobal.com>
2025-12-18 09:28:10 +0530

    Updated File with Examples and Created variables

--- src/01-typescript-introduction/typescript-benefits.ts
+++ src/01-typescript-introduction/typescript-benefits.ts
@@ -69,6 +69,25 @@ interface User {
     isActive: boolean;
 }
 
+//created By Anusha
+const user1: User = {id: 1,name: "Anusha", email: "anusha@example.com",isActive: true};
+function userInfo(user1:User):void{
+    console.log(`User Name: ${user1.name}`);
+    console.log(`User Email: ${user1.email}`);
+    console.log(`User ID: ${user1.id}`);
+}
+
+interface Meeting{
+    meetID:number;
+    meetName:string;
+    meetPassword:string
+}
+function meetingDetails(meet:Meeting):void{
+    console.log(`ID: ${meet.meetID}, Name: ${meet.meetName}, Password: ${meet.meetPassword}`);
+}
+
+meetingDetails({meetID:123, meetName:"Training", meetPassword:"1234567"});
+
 /**
  * Example function showing autocomplete benefits
  * @param {User} user - User object with type information
@@ -95,6 +114,11 @@ enum LocatorStrategy {
     XPATH = 'xpath',
     TEXT = 'text'
 }
+//created By Anusha
+let csslocator:LocatorStrategy= LocatorStrategy.CSS;
+let idlocator:LocatorStrategy= LocatorStrategy.ID;
+let xpathlocator:LocatorStrategy= LocatorStrategy.XPATH;
+let textlocator:LocatorStrategy= LocatorStrategy.TEXT;
 
 /**
  * Web element locator configuration
