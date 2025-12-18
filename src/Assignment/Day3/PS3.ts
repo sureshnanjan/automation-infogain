f6016fde6b956a87c6d432f41e0158daaa8e6867
L Karthikapriya <karthikapriya.L@igglobal.com>
2025-12-18 17:01:53 +0530

    Adding PS3 code

---
+++ src/Assignment/Day3/PS3.ts
@@ -0,0 +1,35 @@
+/*Activity1
+enum MobilePrice { Black, Gold, White }
+
+function calculateAmount(discount: number, color: MobilePrice): number {
+  return color - discount;
+}
+
+const discount = 1;
+
+// By default TS assigns numeric value
+console.log("Actual Price:", MobilePrice.Black);
+console.log("Actual Price:", MobilePrice.Gold);
+console.log("Actual Price:", MobilePrice.White);
+console.log("Final Price after Discount:", calculateAmount(discount, MobilePrice.White));
+*/
+
+//Activity2
+enum MobilePrice {
+  Black,
+  Gold = 30000,
+  White
+}
+
+function calculateAmount(discount: number, color: MobilePrice): number {
+  return color - discount;
+}
+
+const discount = 1000;
+console.log("Actual Price of Mobile(Black,Gold):", MobilePrice.Black,
+     MobilePrice.Gold);
+console.log("Actual Price of White Mobile:", MobilePrice.White);
+console.log("Final Price after Discount:", calculateAmount(discount, MobilePrice.White));
+
+
+
