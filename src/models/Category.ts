a43cfc40e805086b83ba734d582075e8528e192c
mahanteshAPujari <mahantesh.a.pujari@gmail.com>
2025-12-18 23:20:45 +0530

    adding assignments

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,7 @@
 /**
- * Represents a Category entity in the application.
+ * Categories of pet 
  */
-export interface Category {
-    /** Unique identifier for the category */
-    id: number;
-  
-    /** Name of the category */
-    name: string;
-  }
-  
+export interface Category{
+    id:number, // unique identification number
+    name:string // Name of the category
+}
