21d86964d9699e70bd8d1ad82c3261b9fe8dfc87
mahanteshAPujari <mahantesh.a.pujari@gmail.com>
2025-12-18 23:20:45 +0530

    adding assignments

---
+++ src/Assignments/Assignment1/Assignment1A.ts
@@ -0,0 +1,29 @@
+let productId: number = 101;
+let productDescription: string = "Laptop";
+let productName: string = "HP";
+let productAvailable: boolean = true;
+const discountPercentage={Value :20};
+
+var myname: String='Mahantesh'
+
+console.log("Name:",myname);
+console.log("productId:", productId,"and DataType is",typeof productId);
+console.log("productDescription:",productDescription,"and DataType is", typeof productDescription);
+console.log("productName:",productName,"and DataType is", typeof productName);
+console.log("productAvailable:",productAvailable,"and DataType is", typeof productAvailable);
+
+console.log("Discount Percentage:", discountPercentage.Value);
+
+productAvailable = false;
+discountPercentage.Value=30;
+
+console.log("productAvailable:",productAvailable);
+console.log("Discount Percentage:", discountPercentage.Value);
+
+
+
+
+
+
+
+
