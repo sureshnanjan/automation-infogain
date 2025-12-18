91e0c18f9f0c37f4d9a20e3bebe818c0584e1c1c
mahanteshAPujari <mahantesh.a.pujari@gmail.com>
2025-12-18 23:20:45 +0530

    adding assignments

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,14 +1,27 @@
-class HomePage {
-    constructor(parameters) {
-        
-    }
-    this.header: string;
-
-    doLogin(username: string, password: string): boolean {
-        return true;
-    }
-
-    doResetPassowrd(oldpass, newpass): void {
-        
-    }
+enum UserStatus{
+    ACTIVE=1,
+    INACTIVE=2
 }
+interface User {
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phone: string;
+    userStatus: UserStatus;
+  }
+
+  const user1={
+    id:10,
+    userName:"MAP",
+    firstName:"Mahantesh",
+    lastName:"Pujari",
+    email:"M@gmail.com",
+    password:"MAP4203",
+    phone:"+91-4545454545",
+    userStatus:UserStatus.ACTIVE
+  }
+
+  console.log(user1);
