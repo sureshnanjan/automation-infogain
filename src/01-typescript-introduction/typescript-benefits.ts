80d394d4673fc80f1b6a2d260d36b6ef645f8690
ashwini1mohod-droid <ashwini1mohod@gmail.com>
2025-12-18 15:59:47 +0530

    AshwiniCode

--- src/01-typescript-introduction/typescript-benefits.ts
+++ src/01-typescript-introduction/typescript-benefits.ts
@@ -4,7 +4,7 @@
  * @description This file demonstrates why TypeScript is valuable for automation testing
  * and provides practical examples of its benefits over plain JavaScript.
  */
-
+  
 /**
  * WHAT IS TYPESCRIPT?
  * ====================
@@ -402,3 +402,44 @@ export type {
 export {
     AutomationFramework
 };
+
+//Code created by Ashwini
+var productid:number=1001;
+let productdescription:String="Mobile Phone";
+let productname:String="Samsung";
+productname = 12345 as unknown as String 
+let productavailability:boolean=false;
+ 
+const discountpercentage:number=30;
+
+enum Mobileprice{
+  //Black=250,
+  //Gold=280,
+  //Gold=30000,
+  //White=300
+  Black,Gold,White
+}
+
+function calculateAmount(mobilecolor:Mobileprice,discount:number):number{
+  let finalamount:number;
+  finalamount=mobilecolor - (mobilecolor * discount /100);
+  return finalamount;}
+console.log("Final Amount after discount:"+calculateAmount(Mobileprice.Black,discountpercentage));
+console.log("Final Amount after discount:"+calculateAmount(Mobileprice.White,discountpercentage));
+
+console.log("Final Amount after discount:"+calculateAmount(Mobileprice.Gold,discountpercentage));
+console.log("Product Id:"+productid);
+console.log("Product Description:"+productdescription)
+console.log("Product Name:"+productname);
+console.info("Product Availability:" ,productavailability);
+console.log("Product Availability:" ,productavailability);
+console.log("Discount Percentage:"+discountpercentage);
+
+
+
+
+
+
+
+
+
