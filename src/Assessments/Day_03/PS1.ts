41a7e5b2bfa9babe39ad5b037b8876f38bc298af
Manirathnam7 <manirathnam.k@infogain.com>
2025-12-18 19:21:59 +0530

    Day3, Day4 Assessments

---
+++ src/Assessments/Day_03/PS1.ts
@@ -0,0 +1,30 @@
+// Product variables declaration
+let productId: number = 107;
+let productDescription: string = "Wireless Bluetooth Headphones";
+let productName: string = "Sony WH-1000XM5";
+let productAvailable: boolean = true;
+const discountPercentage: number = 15;
+
+// Display data types
+console.log("Type of productId:", typeof productId);
+console.log("Type of productDescription:", typeof productDescription);
+console.log("Type of productName:", typeof productName);
+console.log("Type of productAvailable:", typeof productAvailable);
+
+// Display discount value
+console.log("Discount Percentage:", discountPercentage);
+
+//Activity1
+
+let product_Name : number= 56789;
+
+console.log("Type of productName:", typeof product_Name);
+
+//Activity2
+
+// Updating variable values
+ productAvailable = false;
+
+
+console.log("Updated Product Availability:", productAvailable);
+console.log("Discount Percentage:", discountPercentage);
