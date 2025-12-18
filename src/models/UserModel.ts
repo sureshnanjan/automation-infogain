25db539d171c535d592ca637dad16f2bb70aa1c8
sureshnanjan <sureshnanjan@outlook.com>
2025-12-18 14:00:26 +0530

    Adding Day 4 Topics

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,27 +1,14 @@
-enum UserStatus{
-    ACTIVE=1,
-    INACTIVE=2
+class HomePage {
+    constructor(parameters) {
+        
+    }
+    this.header: string;
+
+    doLogin(username: string, password: string): boolean {
+        return true;
+    }
+
+    doResetPassowrd(oldpass, newpass): void {
+        
+    }
 }
-interface User {
-    id: number;
-    username: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    phone: string;
-    userStatus: UserStatus;
-  }
-
-  const user1={
-    id:10,
-    userName:"MAP",
-    firstName:"Mahantesh",
-    lastName:"Pujari",
-    email:"M@gmail.com",
-    password:"MAP4203",
-    phone:"+91-4545454545",
-    userStatus:UserStatus.ACTIVE
-  }
-
-  console.log(user1);
