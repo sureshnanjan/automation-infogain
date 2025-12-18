f5a26fbb5648b52dd803dbd163668b3140fdb8f7
anushachilaka <chilakaanusha99@gmail.com>
2025-12-18 16:34:03 +0530

    Created UserModel interface

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,14 +1,27 @@
-class HomePage {
-    constructor(parameters) {
-        
-    }
-    this.header: string;
 
-    doLogin(username: string, password: string): boolean {
-        return true;
-    }
-
-    doResetPassowrd(oldpass, newpass): void {
-        
-    }
+/**
+ * UserModel interface representing a user entity.
+ * @interface UserModel
+ */
+interface UserModel {
+    id: number;
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phone: string;
+    userStatus: number;
 }
+
+const userDetails: UserModel = {
+    id: 101,
+    username: "anushaRani",
+    firstName: "Anusha",
+    lastName: "Rani",
+    email: "anusha@example.com", 
+    password: "password123!!",
+    phone: "123-456-7890",
+    userStatus: 1
+}
+console.log(userDetails);
