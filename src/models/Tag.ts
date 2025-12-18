036bf5aeb860fb2837cfcb3a478f3163149659c2
mahanteshAPujari <mahantesh.a.pujari@gmail.com>
2025-12-18 23:20:45 +0530

    adding assignments

--- src/models/Tag.ts
+++ src/models/Tag.ts
@@ -1,15 +1,4 @@
-/**
- * Represents a Tag entity used across the application.
- * 
- * @interface Tag
- * @property {number} id - Unique identifier for the tag.
- * @property {string} name - Display name of the tag.
- */
-export interface Tag {
-    /** Unique identifier for the tag */
-    id: number;
-  
-    /** Display name of the tag */
-    name: string;
-  }
-  
+export interface Tag{
+    id:number
+    name:string
+}
