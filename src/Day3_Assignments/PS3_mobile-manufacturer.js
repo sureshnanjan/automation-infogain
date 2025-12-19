f575f8474ab8963f34f76a3e8b220024776dcf1a
Supraja1992 <suprajapuligandala@gmail.com>
2025-12-19 13:37:14 +0530

    local changes

--- src/Day3_Assignments/PS3_mobile-manufacturer.js
+++ src/Day3_Assignments/PS3_mobile-manufacturer.js
@@ -1,28 +1,26 @@
-function getMobileByManufacturer(manufacturer: string): string[] {
-    let mobileList: string[] = [];
-
+function getMobileByManufacturer(manufacturer) {
+    var mobileList = [];
     if (manufacturer === "Apple") {
         mobileList = ["iPhone 13", "iPhone 14", "iPhone 15"];
-    } else if (manufacturer === "Samsung") {
+    }
+    else if (manufacturer === "Samsung") {
         mobileList = ["Galaxy S22", "Galaxy S23", "Galaxy A54"];
-    } else if (manufacturer === "Nokia") {
+    }
+    else if (manufacturer === "Nokia") {
         mobileList = ["Nokia 3310", "Nokia G21", "Nokia X30"];
-    } else {
+    }
+    else {
         mobileList = ["No mobiles available"];
     }
-//Activity 1: Comment Specific Lines
-// • Re-execute the code. 
-// • Observe how commenting these lines affects: 
-// o Function execution 
-// o Returned output 
-// o Console display  
-
+    //Activity 1: Comment Specific Lines
+    // • Re-execute the code. 
+    // • Observe how commenting these lines affects: 
+    // o Function execution 
+    // o Returned output 
+    // o Console display  
     return mobileList;
 }
-
 // Function call
 //console.log("The available mobile list: " + getMobileByManufacturer("Apple"));
-
 // Modifying the function call
 console.log("The available mobile list:" + getMobileByManufacturer("Nokia"));
-
---
+++ src/Day4_Assignments/PS4_Order-Details.js
@@ -0,0 +1,13 @@
+// Create an order object missing one mandatory property.
+// let order1 = {
+//     orderId: 101,
+//     customerName: "Ravi"
+// };
+var order1 = {
+    orderId: 101,
+    customerName: "Ravi",
+    orderAmount: 3500
+};
+console.log("Order Id:", order1.orderId);
+console.log("Customer Name:", order1.customerName);
+console.log("Order Amount:", order1.orderAmount);
