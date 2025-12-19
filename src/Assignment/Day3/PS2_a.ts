fce7be8b3381374845bd49fa300210f5cac0dd3f
L Karthikapriya <karthikapriya.L@igglobal.com>
2025-12-19 11:38:38 +0530

    Adding latest

--- src/Assignment/Day3/PS2_a.ts
+++ src/Assignment/Day3/PS2_a.ts
@@ -16,36 +16,3 @@ let finalPrice = calculateAmount(MobilePrice.White, discount);
 
 console.log("Actual Price:", MobilePrice.White);
 console.log("Final Price after discount:", finalPrice); 
-
-//Activity1
-
-/*enum MobilePrice { Black, Gold, White }
-
-function calculateAmount(color: MobilePrice, discount: number): number {
-  return color - discount;
-}
-
-console.log("Actual Price:", MobilePrice.White);
-console.log("Final Price:", calculateAmount(MobilePrice.White, 1));
-*/
-
-//Activity2
-
-enum MobilePrice {
-  Black,
-  Gold = 30000,
-  White
-}
-
-function calculateAmount(color: MobilePrice, discount: number): number {
-  return color - discount;
-}
-
-let discount = 1000;
-
-console.log("Actual Price of White Mobile:", MobilePrice.White);
-console.log(
-  "Final Price after Discount:",
-  calculateAmount(MobilePrice.White, discount)
-);
-
--- src/Assignment/Day3/PS2_b.ts
+++ src/Assignment/Day3/PS2_b.ts
@@ -1,11 +1,15 @@
 //Activity1
 
-enum MobilePrice1{ Black1,Gold1,White1}
-
-function calculateAmount1   (color: MobilePrice, discount1  : number): number {
-    let totalAmount = color - discount1;
-  return totalAmount;
+enum MobilePrice1{
+     Black1,
+     Gold1,
+     White1
 }
 
-console.log("Actual Price:", MobilePrice.White1);
-console.log("Final Price:", calculateAmount1(MobilePrice.White1,1));
+function calculateAmount1   (color: MobilePrice1, discount1  : number): number {
+    let totalAmount1= color - discount1;
+  return totalAmount1;
+}
+
+console.log("Actual Price:", MobilePrice1.White1);
+console.log("Final Price:", calculateAmount1(MobilePrice1.White1 ,1));
