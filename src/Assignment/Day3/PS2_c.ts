43a00f0edb9726a88609a9b2413bc5e489a60df0
L Karthikapriya <karthikapriya.L@igglobal.com>
2025-12-19 11:38:38 +0530

    Adding latest

--- src/Assignment/Day3/PS2_c.ts
+++ src/Assignment/Day3/PS2_c.ts
@@ -1,20 +1,20 @@
 //Activity2
 
-enum MobilePrice {
+enum MobilePrice2{
   Black2,
   Gold2= 30000,
   White2
 }
 
-function calculateAmount2   (color: MobilePrice, discount: number): number {
-  let totalAmount2= color - discount;
+function calculateAmount2   (color: MobilePrice2, discount2 : number): number {
+  let totalAmount2= color - discount2;
   return totalAmount2;
 }
 
 let discount2= 1000;
 
-console.log("Actual Price of White Mobile:", MobilePrice.White2);
+console.log("Actual Price of White Mobile:", MobilePrice2.White2);
 console.log(
   "Final Price after Discount:",
-  calculateAmount(MobilePrice.White, discount2));
+  calculateAmount2(MobilePrice2.White2, discount2));
 
