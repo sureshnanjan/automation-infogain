bd8874f9a8f1d9e9252887a867e7773f5c9a3a5a
L Karthikapriya <karthikapriya.L@igglobal.com>
2025-12-19 10:53:57 +0530

    PS3.ts

--- src/Assignment/Day3/PS3.ts
+++ src/Assignment/Day3/PS3.ts
@@ -26,8 +26,7 @@ function calculateAmount(discount: number, color: MobilePrice): number {
 }
 
 const discount = 1000;
-console.log("Actual Price of Mobile(Black,Gold):", MobilePrice.Black,
-     MobilePrice.Gold);
+console.log("Actual Price of Mobile(Black,Gold):", MobilePrice.Black, MobilePrice.Gold);
 console.log("Actual Price of White Mobile:", MobilePrice.White);
 console.log("Final Price after Discount:", calculateAmount(discount, MobilePrice.White));
 
