11f7664dd6fdfe38e57d69dc0f78a45d3fdbee5f
Saravan Muthusamy <Saravan.muthusamy@igglobal.com>
2025-12-21 23:09:34 +0530

    Day4 Assignment

--- src/01-typescript-introduction/typescript-saravan-practise.ts
+++ src/01-typescript-introduction/typescript-saravan-practise.ts
@@ -199,5 +199,219 @@ console.log("The available mobile list:" + getMobileByManufacturer("Nokia"));
 
 //Problem Statement #6
 
+function getMobileByManufacturer1(manufacturer: string = "Samsung",id?: number): string[] {
+
+  // Special condition: if id is 101, return Moto list
+  if (id === 101) {
+    return ["Moto G52", "Moto Edge 40", "Moto Razr"];
+  }
+
+  // Manufacturer-based logic
+  if (manufacturer === "Samsung") {
+    return ["Galaxy S23", "Galaxy A54", "Galaxy Z Fold"];
+  } else if (manufacturer === "Apple") {
+    return ["iPhone 14", "iPhone 15", "iPhone SE"];
+  } else {
+    return ["No mobiles available"];
+  }
+}
+
+// Normal function call
+console.log("The available mobile list :", getMobileByManufacturer1("Apple"));
 
 
+//Activity 1: Modify Function Declaration
+
+function getMobileByManufacturer2(manufacturer, id?: number): string[]
+{
+  // Special condition: if id is 101, return Moto list
+  if (id === 101) {
+    return ["Moto G52", "Moto Edge 40", "Moto Razr"];
+  }
+
+  // Manufacturer-based logic
+  if (manufacturer === "Samsung") {
+    return ["Galaxy S23", "Galaxy A54", "Galaxy Z Fold"];
+  } else if (manufacturer === "Apple") {
+    return ["iPhone 14", "iPhone 15", "iPhone SE"];
+  } else {
+    return ["No mobiles available"];
+  }
+}
+
+// Normal function call
+console.log("The available mobile list :", getMobileByManufacturer1("Apple"));
+
+//Activity 2: Modify Function Invocation
+
+console.log("The available mobile list : " + getMobileByManufacturer1(undefined));  //Argument of type 'undefined' is not assignable to parameter of type 'string'.
+//console.log("The available mobile list : " + getMobileByManufacturer1(101)); //Argument of type 'number' is not assignable to parameter of type 'string'.
+console.log("The available mobile list : " + getMobileByManufacturer1(undefined, 102)); // No mobiles available
+
+/********************************************************************************* */
+
+
+//Assignment Date-18/12/2025
+
+//Problem Statement #1
+
+// Step 1: Declare cart as string array
+let cart: string[] = [];
+
+// Step 2: Function to add multiple products
+function addToCart(productNames: string[]): string[] {
+  for (let product of productNames) {
+    cart.push(product);
+  }
+  return cart;
+}
+
+// Step 3: Correct function call
+console.log(
+  "Cart Items are: " +
+    addToCart(["Moto G Play, 4th Gen", "Apple iPhone 5s"]).join(", ")
+);
+
+
+
+//Problem Statement #2
+
+// Step 1:Create an interface Product
+interface Product {
+  productId: number;
+  productName: string;
+}
+
+//Create a function
+function printProductDetails(product: Product): void {
+  console.log("Product ID:", product.productId);
+  console.log("Product Name:", product.productName);
+}
+
+
+// let product: Product = {
+//   productName: "iPhone 14"
+// };
+
+
+let product: Product = {
+  productId: 101,
+  productName: "iPhone 14"
+};
+
+printProductDetails(product);
+
+
+let productWithPrice = {
+  productId: 102,
+  productName: "Samsung Galaxy S23",
+  productPrice: 750
+};
+
+printProductDetails(productWithPrice);
+
+
+//Problem Statement #3
+
+// Declare the CartItem interface.
+interface CartItem {
+  itemId: number;
+  itemName: string;
+  quantity: number;
+}
+
+//Create multiple cart item objects.
+
+const item1: CartItem = {
+  itemId: 101,
+  itemName: "Laptop",
+  quantity: 1
+};
+
+const item2: CartItem = {
+  itemId: 102,
+  itemName: "Mouse",
+  quantity: 2
+};
+
+const item3: CartItem = {
+  itemId: 103,
+  itemName: "Keyboard",
+  quantity: 1
+};
+
+//Pass them to a function and display the total quantity.
+
+function calculateTotalQuantity(items: CartItem[]): void {
+  let totalQuantity = 0;
+
+  for (let item of items) {
+    totalQuantity += item.quantity;
+  }
+
+  console.log("Total Quantity of Items in Cart:", totalQuantity);
+}
+//Ensure the code compiles and executes without TypeScript errors.
+
+calculateTotalQuantity([item1, item2, item3]);
+
+//Problem Statement #4
+
+//Declare the Order interface.
+interface Order {
+  orderId: number;
+  customerName: string;
+  orderAmount: number;
+}
+
+//Create an order object missing one mandatory property.
+
+// const order: Order = {
+//   orderId: 5001,
+//   customerName: "John Doe"
+// };
+
+
+//Fix the object to satisfy the interface.
+
+const order: Order = {
+  orderId: 5001,
+  customerName: "John Doe",
+  orderAmount: 1250
+};
+
+//Display order details on the console.
+
+function displayOrderDetails(order: Order): void {
+  console.log("Order ID:", order.orderId);
+  console.log("Customer Name:", order.customerName);
+  console.log("Order Amount:", order.orderAmount);
+}
+
+displayOrderDetails(order);
+
+
+//Problem Statement #5
+
+// Declare the Customer interface.
+
+interface Customer {
+  customerId: number;
+  customerName: string;
+}
+
+//Create a customer object with an extra property accountType.
+const customerDetails = {
+  customerId: 1001,
+  customerName: "Ravi Kumar",
+  accountType: "Savings" // Extra property
+};
+
+//Pass the object to the function and display valid details only.
+function displayCustomerDetails(customer: Customer): void {
+  console.log("Customer ID:", customer.customerId);
+  console.log("Customer Name:", customer.customerName);
+}
+
+
+displayCustomerDetails(customerDetails);
