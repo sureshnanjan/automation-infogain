3ec9b84847f60d252acf0c5bf02affb4ded7a048
mahanteshAPujari <mahantesh.a.pujari@gmail.com>
2025-12-22 17:24:21 +0530

    adding new files commit
    adding changes

--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -12,12 +12,4 @@ import { Tag } from "src/models/Tag";
     photoUrls: string[];                     // Required field
     tags: Tag[];                             // List of tags
     status: "available" | "pending" | "sold";// Pet status in store
-<<<<<<< HEAD
-<<<<<<< HEAD
  }
-=======
-  }
->>>>>>> 57ee0e0 (adding assignments)
-=======
-  }
->>>>>>> 91e0c18 (adding assignments)
