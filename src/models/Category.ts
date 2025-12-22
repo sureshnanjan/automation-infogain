5e882a9a43e21d4689722588551639a20814ebb3
mahanteshAPujari <mahantesh.a.pujari@gmail.com>
2025-12-22 17:24:21 +0530

    adding new files commit
    adding changes

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -12,7 +12,11 @@ export interface Category {
 /**
  * Represents a Category entity in the application.
  */
-export interface Category{
-    id:number, // unique identification number
-    name:string // Name of the category
-}
+export interface Category {
+    /** Unique identifier for the category */
+    id: number;
+  
+    /** Name of the category */
+    name: string;
+  }
+  
