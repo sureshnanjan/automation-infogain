6ffd3d928d438fe6701d3f596c97ea0ee250cf92
mahanteshAPujari <mahantesh.a.pujari@gmail.com>
2025-12-29 22:51:11 +0530

    Added Day4 TypeScript assignments and Disappearing Elements POM tests

--- src/test-scenarios/DisappearingElements-tests.ts
+++ src/test-scenarios/DisappearingElements-tests.ts
@@ -5,22 +5,9 @@ import {getHerokuAppUrl,getHerokuApp} from '@src/utilities/herokuapp-utils';
 
 
 test("Verify DisappearingElements Link exists",async({page})=>{
-    const homePage:HomePageOperations = getHerokuApp(page);
-    const title=await homePage.getTitle();
-    expect(title).toBe("Welcome to the-internet");
+
  
 
 });
 
-test("Verify DisappearingElements page is clickable",async({page})=>{
-    const homePage:HomePageOperations = getHerokuApp(page);
-    const disappearingelements:DisappearingElementsOperations = getDisappearingElementsPage(page);
-    await disappearingelements.clickDisappearingElementsLink();
-});
-
-test("Verify DisappearingElements page title",async({page})=>{
-    const disappearingelements:DisappearingElementsOperations = getDisappearingElementsPage(page);
-    await disappearingelements.clickDisappearingElementsLink();
-    await disappearingelements.verifyDisappearingElementsPageTitle();
-});
 
