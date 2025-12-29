df13a051695f635c1acec73c1ec80b1f3a879413
L Karthikapriya <karthikapriya.L@igglobal.com>
2025-12-29 08:41:14 +0530

    latest code

--- src/utilities/digestauth-utils.ts
+++ src/utilities/digestauth-utils.ts
@@ -1,5 +1,6 @@
 export const DIGEST_AUTH = {
 <<<<<<< HEAD
+<<<<<<< HEAD
   username: process.env.DIGEST_USER ?? 'admin',
   password: process.env.DIGEST_PASS ?? 'admin',
 };
@@ -15,3 +16,11 @@ export function getDigestAuthUrl(username: string, password: string): string {
   return `https://${username}:${password}@the-internet.herokuapp.com/digest_auth`;
 }
 >>>>>>> b690760 (digest auth)
+=======
+  username: process.env.DIGEST_USER ?? 'admin',
+  password: process.env.DIGEST_PASS ?? 'admin',
+};
+
+export const DIGEST_AUTH_BASE_URL =
+  'https://the-internet.herokuapp.com/digest_auth';
+>>>>>>> 8907107 (latest code)
