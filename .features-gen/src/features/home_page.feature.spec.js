b120be9661f300d15a94bd3031ff3edf8a301069
sureshnanjan <sureshnanjan@outlook.com>
2025-12-30 11:20:28 +0530

    BDD Implementation for Heroku Page

---
+++ .features-gen/src/features/home_page.feature.spec.js
@@ -0,0 +1,24 @@
+// Generated from: src\features\home_page.feature
+import { test } from "playwright-bdd";
+
+test.describe('Heroku App Home Page functionality', () => {
+
+  test('Home Page has a valid Title', async ({ Given, When, Then, page }) => { 
+    await Given('User Visits HomePage', null, { page }); 
+    await When('User Observes Title'); 
+    await Then('title should match "Welcome to the-internet"'); 
+  });
+
+});
+
+// == technical section ==
+
+test.use({
+  $test: [({}, use) => use(test), { scope: 'test', box: true }],
+  $uri: [({}, use) => use('src\\features\\home_page.feature'), { scope: 'test', box: true }],
+  $bddFileData: [({}, use) => use(bddFileData), { scope: "test", box: true }],
+});
+
+const bddFileData = [ // bdd-data-start
+  {"pwTestLine":6,"pickleLine":4,"tags":[],"steps":[{"pwStepLine":7,"gherkinStepLine":5,"keywordType":"Context","textWithKeyword":"Given User Visits HomePage","stepMatchArguments":[]},{"pwStepLine":8,"gherkinStepLine":6,"keywordType":"Action","textWithKeyword":"When User Observes Title","stepMatchArguments":[]},{"pwStepLine":9,"gherkinStepLine":7,"keywordType":"Outcome","textWithKeyword":"Then title should match \"Welcome to the-internet\"","stepMatchArguments":[{"group":{"start":19,"value":"\"Welcome to the-internet\"","children":[{"start":20,"value":"Welcome to the-internet","children":[{"children":[]}]},{"children":[{"children":[]}]}]},"parameterTypeName":"string"}]}]},
+]; // bdd-data-end
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
+import { defineBddConfig } from 'playwright-bdd';
+const testDir = defineBddConfig({
+  features: 'src/features/**/*.feature',
+  steps: 'src/features/step-definitions/**/*.ts',
+});
 
 /**
  * Read environment variables from file.
@@ -12,8 +17,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './src/test-scenarios',
-  testMatch: '**/*.ts',
+  //testDir: './src/test-scenarios',
+  testDir: testDir,
+  //testMatch: '**/*.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,7 +29,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['json', { outputFile: 'test-results/cucumber-report.json' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
