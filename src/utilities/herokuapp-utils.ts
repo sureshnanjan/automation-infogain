19e2412eaa37ef28b02ee42f8ffb0babe76ebee8
mahanteshAPujari <mahantesh.a.pujari@gmail.com>
2025-12-30 11:32:43 +0530

    Adding Changes

--- src/utilities/herokuapp-utils.ts
+++ src/utilities/herokuapp-utils.ts
@@ -1,7 +1,6 @@
 import { HomePageOperations } from "@src/operations/HomePageOperations";
 import { HomePage } from "@src/web-implementation/HomePage";
 import { Page } from "@playwright/test";
-import { Logger } from "./Logger";
 export function getHerokuAppUrl(): string {
     // Env Files
     // URL Factory - testing , stshing, production, customer , 
@@ -11,9 +10,5 @@ export function getHerokuAppUrl(): string {
 export async function getHerokuApp(page:Page): Promise<HomePageOperations> {
     // App Factory
     // How call a async factory method from here?
-    const myLogger:Logger = Logger.createInstance()
-    return  HomePage.create(page);
-    //app.navigate();
-    //return app;
-
+    return new HomePage(page);
 }
