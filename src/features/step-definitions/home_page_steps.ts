c88299f7e52717886edde6c59cdeb05fde0a3dc6
Pavithra Murugesan <Pavithra.Murugesan@igglobal.com>
2026-01-07 13:10:07 +0530

    API

--- src/features/step-definitions/home_page_steps.ts
+++ src/features/step-definitions/home_page_steps.ts
@@ -4,39 +4,40 @@ import { createBdd } from "playwright-bdd";
 import { expect } from "@playwright/test";
 
 const { Given, When, Then } = createBdd(); // Decorators
-let homePage:HomePageOperations;
-let actualResult:string|null;
-let expected_logo:{color:string, place: string, font:number}
-Given('User Visits HomePage', async ({page}) => {
 let homePage: HomePageOperations;
 let actualResult: string | null;
-
+let expected_logo: { color: string; place: string; font: number };
 Given("User Visits HomePage", async ({ page }) => {
-  // Step: Given User Visits HomePage
-  // From: src\features\home_page.feature:5:1
-  homePage = await getHerokuApp(page);
-});
+  let homePage: HomePageOperations;
+  let actualResult: string | null;
 
-When("User Observes Title", async ({}) => {
-  // Step: When User Observes Title
-  // From: src\features\home_page.feature:6:1
-  actualResult = await homePage.getTitle();
-});
+  Given("User Visits HomePage", async ({ page }) => {
+    // Step: Given User Visits HomePage
+    // From: src\features\home_page.feature:5:1
+    homePage = await getHerokuApp(page);
+  });
 
-Then("title should match {string}", async ({}, arg) => {
-  // Step: Then title should match "Welcome to the-Internet"
-  // From: src\features\home_page.feature:7:1
-  expect(actualResult).toEqual(arg);
-});
+  When("User Observes Title", async ({}) => {
+    // Step: When User Observes Title
+    // From: src\features\home_page.feature:6:1
+    actualResult = await homePage.getTitle();
+  });
 
-When("User Observes Subtitle", async ({}) => {
-  // Step: When User Observes Subtitle
-  // From: src\features\home_page.feature:11:1
-  actualResult = await homePage.getSubTitle();
-});
+  Then("title should match {string}", async ({}, arg) => {
+    // Step: Then title should match "Welcome to the-Internet"
+    // From: src\features\home_page.feature:7:1
+    expect(actualResult).toEqual(arg);
+  });
 
-Then("Subtitle should match {string}", async ({}, arg: string) => {
-  // Step: Then Subtitle should match "Available Examples"
-  // From: src\features\home_page.feature:12:1
-  expect(actualResult).toEqual(arg);
+  When("User Observes Subtitle", async ({}) => {
+    // Step: When User Observes Subtitle
+    // From: src\features\home_page.feature:11:1
+    actualResult = await homePage.getSubTitle();
+  });
+
+  Then("Subtitle should match {string}", async ({}, arg: string) => {
+    // Step: Then Subtitle should match "Available Examples"
+    // From: src\features\home_page.feature:12:1
+    expect(actualResult).toEqual(arg);
+  });
 });
