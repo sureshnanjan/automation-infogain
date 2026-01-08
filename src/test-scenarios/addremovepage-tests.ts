f0ac1a95146840f2b60fe76a0ea0844e6a78539f
Welcome2Breville4120 <anju.pillai@breville.com>
2026-01-08 18:56:39 +0530

    adding files to branch

--- src/test-scenarios/addremovepage-tests.ts
+++ src/test-scenarios/addremovepage-tests.ts
@@ -1,32 +1,19 @@
 import {test,expect} from '@playwright/test';
-import {AddRemovePage} from '../web-implementation/AddRemovePage';
-import {AddRemoveOperations} from '@src/operations/AddRemoveOperations';
-import {getHerokuAppUrl,getHerokuApp} from '@src/utilities/herokuapp-utils';
-import { HomePageOperations } from '@src/operations/HomePageOperations';
-import { getAddRemoveTestingApp } from '@src/utilities/addremove-utils';
-
-// test("Verify Home Page Title",async({page})=>{
-//     const homePage:HomePageOperations = await getHerokuApp(page);
-//     const title=await homePage.getTitle();
-//     expect(title).toBe("Welcome to the-internet");
-// });
+import { AddRemovePage } from '../web-implementation/AddRemovePage';
+import {AddRemoveOperations} from '../operations/AddRemoveOperations';
 
 test("Verify Add Remove Heading",async({page})=>{
     const addRemovePage: AddRemoveOperations = new AddRemovePage(page);
-    const expectedHeading='Add/Remove Elements';
     await addRemovePage.gotoExampleAddRemove();
+    const expectedHeading='Add/Remove Elements';
     const heading=await addRemovePage.getHeading();
-    console.log('Page heading is:', heading);
     expect(heading).toBe(expectedHeading);
-    await page.close();
-    
 });
 
 test("Add element",async({page})=>{
     const addRemovePage: AddRemoveOperations = new AddRemovePage(page);
     await addRemovePage.gotoExampleAddRemove();
     await addRemovePage.addElement();
-    await page.close();
 });
 
 test("Remove element after adding the element",async({page})=>{
@@ -34,21 +21,18 @@ test("Remove element after adding the element",async({page})=>{
     await addRemovePage.gotoExampleAddRemove();
     await addRemovePage.addElement();
     await addRemovePage.removeElement();
-    await page.close();
 }); 
 
 test("Add multiple elements",async({page})=>{
     const addRemovePage: AddRemoveOperations = new AddRemovePage(page);
     await addRemovePage.gotoExampleAddRemove();
     await addRemovePage.addMultipleElements(5);
-    await page.close();
 });
 
 test("Get count of elements added",async({page})=>{
     const addRemovePage: AddRemoveOperations = new AddRemovePage(page);
     await addRemovePage.gotoExampleAddRemove();
     await addRemovePage.addMultipleElements(4);
-    const countOfAddedElements=addRemovePage.getAvailableElements();
+    const countOfAddedElements=await addRemovePage.getAvailableElements();
     expect(countOfAddedElements).toBe(4);
-    await page.close();
 });
